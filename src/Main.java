36a9222e60735f949a3260ec3df9a4dd47462731
Sara Chalabi <chalabi85@gmail.com>
2021-10-10 08:23:07 +0300

    Add comment to say hello

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
         System.out.println("Hello, My name is Tyler");
         System.out.println("This line is garbage and needs to be removed.");
+        System.out.println("Hello, I am Sara");
 
     }
 }
